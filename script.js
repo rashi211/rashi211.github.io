69c9eacc3a0ee22273985039d60f9f55019a9698
rashi211 <50420490+rashi211@users.noreply.github.com>
2026-03-05 03:42:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.1 });
+
+cards.forEach(card => {
+  observer.observe(card);
+});
